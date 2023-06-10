0c8700c24ed7c44e7e64f02bd03c29cd325e6d47
randi1972 <115630639+randi1972@users.noreply.github.com>
2023-06-10 23:44:06 +0700

    Add files via upload

---
+++ DataDiri.java
@@ -0,0 +1,30 @@
+package vidio5;
+
+public class DataDiri{
+
+    public static void main(String[] args) {
+        //membuat variabel
+        /*
+            -format penulisan variabel
+              <tipe data> namaVariabel
+         */
+
+         String nama, alamat;
+         int usia;
+         Double tinggi;
+
+         //mengisi variabel
+         nama = "randiansyah";
+         alamat = "sukabumi, parakansalak";
+         usia = 20;
+         tinggi = 165.5;
+
+         //mencetak ke layar isi variabel
+         System.out.println("Nama = " + nama);
+         System.out.println("Alamat = " + alamat);
+         System.out.println("Usia = " + usia);
+         System.out.println("Tinggi = " + tinggi);
+
+    }
+
+}    
